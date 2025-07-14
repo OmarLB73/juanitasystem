794bbbff58e0c454524a6c48cecca1945d559668
OmarLB73 <omar.lazo.b@gmail.com>
2025-07-14 01:31:34 -0400

    v1.012 Link calendario.

--- proyect/urls.py
+++ proyect/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('panel/', views.panel_view, name='panel_url'), #(nombre en la url navegador, nombre de la vista en el archivo views.py, nombre de la variable que se usa en la vista)
     path('new/', views.proyect_new, name='proyect_new_url'), 
     path('view/<int:proyect_id>/', views.proyect_view, name='view_url'), 
+    path('calendar/', views.panel_view, name='calendar_url'), #(nombre en la url navegador, nombre de la vista en el archivo views.py, nombre de la variable que se usa en la vista)
     path('grafics/', views.grafics_view, name='grafics_url'), 
 
         
